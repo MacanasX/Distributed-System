445f0f04fd68e51e02848fad31c9db1c5d5e4c02
gross <luca.b.gross@stud.h-da.de>
2022-05-12 17:37:15 +0200

    Client,Server without Docker

--- Sensor/src/Sensor.java
+++ Sensor/src/Sensor.java
@@ -1,2 +1,57 @@
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.sql.Timestamp;
+import org.json.simple.JSONObject;
+
+
 public class Sensor {
+
+    private int myID;
+    private String sensorName;
+    private String type;
+    private SensorData mydata;
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+
+    Sensor(int myID,String sensorName,String type)
+    {
+        this.myID = myID;
+        this.sensorName = sensorName;
+        this.type = type;
+    }
+
+    public int getMyID() {
+        return myID;
+    }
+
+    public void setMyID(int myID) {
+        this.myID = myID;
+    }
+
+    public String getSensorName() {
+        return sensorName;
+    }
+
+    public void setSensorName(String sensorName) {
+        this.sensorName = sensorName;
+    }
+
+
+
+
+
+
+
 }
+
+
