d10a470e50da6ffb3343c26215a2ccffc061e6c1
Berneiser <florian.berneiser@stud.h-da.de>
2022-06-21 01:36:14 +0200

    updated database service

--- HTTP-Server/src/main/java/ThriftClient.java
+++ HTTP-Server/src/main/java/ThriftClient.java
@@ -17,44 +17,48 @@ public class ThriftClient extends  Thread {
 
   public void run(){
 
-    try (TTransport transport = new TSocket(HOST, PORT)){
-      transport.open();
-      TProtocol protocol = new TBinaryProtocol(transport);
-      CRUD.Client client  = new CRUD.Client(protocol);
-      //CRUD.Client client1 = new Client(protocol);
-        try (TTransport transport2 = new TSocket(HOST2, PORT)){
+
+    while(true) {
+
+      try (TTransport transport = new TSocket(HOST, PORT)) {
+        transport.open();
+        TProtocol protocol = new TBinaryProtocol(transport);
+        CRUD.Client client = new CRUD.Client(protocol);
+        //CRUD.Client client1 = new Client(protocol);
+        try (TTransport transport2 = new TSocket(HOST2, PORT)) {
           transport2.open();
+
           TProtocol protocol2 = new TBinaryProtocol(transport2);
-          CRUD.Client client2  = new CRUD.Client(protocol2);
+          CRUD.Client client2 = new CRUD.Client(protocol2);
 
 
-              while(true) {
-                //Call RPC for Database
-                Sensor temp = HTTPServer.myReceivedMessages.take();
-               // client.insert(HTTPServer.myReceivedMessages.take());
+          while (true) {
+            //Call RPC for Database
+            if (transport.isOpen() && transport2.isOpen()) {
 
-                client.insert(temp);
-                client2.insert(temp);
+              Sensor temp = HTTPServer.myReceivedMessages.take();
+              // client.insert(HTTPServer.myReceivedMessages.take());
+              client.insert(temp);
+              client2.insert(temp);
+            } else {
+              break;
+            }
+            // Test calls for functions
+            // client.update(database);
+            // System.out.println("Select Methode called : " + client.select(database));
+            // System.out.println("Remove called : " +  client.remove(database));
+            // System.out.println("Remove called second time  : " +  client.remove(database));
 
-                // Test calls for functions
-                // client.update(database);
-                // System.out.println("Select Methode called : " + client.select(database));
-                // System.out.println("Remove called : " +  client.remove(database));
-                // System.out.println("Remove called second time  : " +  client.remove(database));
-
-              }
-
-          } catch (TException | InterruptedException e2) {
-            e2.printStackTrace();
           }
 
-    } catch (TException e) {
-      e.printStackTrace();
+        } catch (TException | InterruptedException e2) {
+          System.out.println("Meldung: Datenbank2 ist nicht erreichbar2");
+        }
+
+      } catch (TException e) {
+        System.out.println("Meldung: Datenbank1 ist nicht erreichbar");
+      }
+
     }
-
-
-
   }
-
-
 }
