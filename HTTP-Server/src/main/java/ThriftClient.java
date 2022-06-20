f45f2c7ccc903053ddc90280e03c1fdc245834b7
Berneiser <florian.berneiser@stud.h-da.de>
2022-06-20 14:50:44 +0200

    added second database service and container

--- HTTP-Server/src/main/java/ThriftClient.java
+++ HTTP-Server/src/main/java/ThriftClient.java
@@ -1,5 +1,6 @@
 import databaseclient.CRUD;
 import databaseclient.CRUD.Client;
+import databaseclient.Sensor;
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
@@ -11,6 +12,7 @@ public class ThriftClient extends  Thread {
   public static final int PORT = 9090;
   /** The host the client connects to. */
   public static final String HOST = "Database";
+  public static final String HOST2 = "Database2";
 
 
   public void run(){
@@ -20,22 +22,36 @@ public class ThriftClient extends  Thread {
       TProtocol protocol = new TBinaryProtocol(transport);
       CRUD.Client client  = new CRUD.Client(protocol);
       //CRUD.Client client1 = new Client(protocol);
+        try (TTransport transport2 = new TSocket(HOST2, PORT)){
+          transport2.open();
+          TProtocol protocol2 = new TBinaryProtocol(transport2);
+          CRUD.Client client2  = new CRUD.Client(protocol2);
 
 
-      while(true) {
-        //Call RPC for Database
-        client.insert(HTTPServer.myReceivedMessages.take());
+              while(true) {
+                //Call RPC for Database
+                Sensor temp = HTTPServer.myReceivedMessages.take();
+               // client.insert(HTTPServer.myReceivedMessages.take());
 
-        // Test calls for functions
-        // client.update(database);
-        // System.out.println("Select Methode called : " + client.select(database));
-        // System.out.println("Remove called : " +  client.remove(database));
-        // System.out.println("Remove called second time  : " +  client.remove(database));
+                client.insert(temp);
+                client2.insert(temp);
 
-      }
-    } catch (TException | InterruptedException e) {
+                // Test calls for functions
+                // client.update(database);
+                // System.out.println("Select Methode called : " + client.select(database));
+                // System.out.println("Remove called : " +  client.remove(database));
+                // System.out.println("Remove called second time  : " +  client.remove(database));
+
+              }
+
+          } catch (TException | InterruptedException e2) {
+            e2.printStackTrace();
+          }
+
+    } catch (TException e) {
       e.printStackTrace();
     }
+
 
 
   }
